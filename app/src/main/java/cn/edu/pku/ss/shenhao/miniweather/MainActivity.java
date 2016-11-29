4deb981d5c58459e8611b5e00982d608580cec9e
striker8805 <s271693408@hotmail.com>
2016-11-29 10:11:59 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/ss/shenhao/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.ss.shenhao.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by striker on 2016/11/29.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
